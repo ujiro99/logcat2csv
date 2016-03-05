247eb982b9f97fe374c26ce20aa9e2eeb18e53f5
ujiro99 <siro.cola@gmail.com>
2016-03-05 23:26:49 +0900

    add testcase

--- logcat2csv_test.go
+++ logcat2csv_test.go
@@ -36,8 +36,33 @@ func TestLogcat2csv_Exec_File(t *testing.T) {
 	logcat2csv := logcat2csv{}
 	logcat2csv.Exec(params)
 
+	checkFile(paths[0], expect, t)
+}
+
+func TestLogcat2csv_Exec_Multiple_File(t *testing.T) {
+	expect0 := []string{
+		"01-01 00:00:00.000,930,931,I,tag_value,message_value_1",
+		"01-01 00:00:01.000,930,931,I,tag_value,message_value_2",
+	}
+	expect1 := []string{
+		"01-01 00:00:00.000,930,931,I,tag_value,message_value_3",
+		"01-01 00:00:01.000,930,931,I,tag_value,message_value_4",
+	}
+	paths := []string{"./test/logcat.txt", "./test/logcat2.txt"}
+	params := cmdParams{
+		paths: paths,
+	}
+
+	logcat2csv := logcat2csv{}
+	logcat2csv.Exec(params)
+
+	checkFile(paths[0], expect0, t)
+	checkFile(paths[1], expect1, t)
+}
+
+func checkFile(file string, expect []string, t *testing.T) {
 	var out string
-	fp, err := os.Open("./test/logcat.txt.csv")
+	fp, err := os.Open(file + ".csv")
 	if err != nil {
 		t.Errorf("os.Open: %v", err)
 	}
@@ -55,6 +80,6 @@ func TestLogcat2csv_Exec_File(t *testing.T) {
 	if err := scanner.Err(); err != nil {
 		t.Errorf("scanner.Err: %v", err)
 	} else {
-		os.Remove("./test/logcat.txt.csv")
+		os.Remove(file + ".csv")
 	}
 }
